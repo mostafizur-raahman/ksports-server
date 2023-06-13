332f971790cdc95774581d99353282dcc526a94c
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-06-13 16:05:31 +0600

    payment method work done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -113,7 +114,7 @@ async function run() {
         res.send(result);
     })
 
-    app.post('/classes',async(req,res)=>{
+    app.post('/classes',verifyJWT,async(req,res)=>{
       const newClass = req.body;
       const result = await classCollections.insertOne(newClass);
       res.send(result);
@@ -153,7 +154,24 @@ async function run() {
       res.send(result);
 
     })
+    // create payment intent
+    app.post('/create-payment-intent',async(req,res)=>{
+      const {price} = req.body;
+      const amount = price * 100;
+      console.log(price,amount);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency :'usd',
+        payment_method_types:[
+          'card'
+        ]
+      })
 
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+
+    })
 
    
     await client.db("admin").command({ ping: 1 });
